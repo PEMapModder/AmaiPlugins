dd444d64efb661a3bbd4f4c9f0c4e67466b0dc65
PEMapModder <pemapmodder1970@gmail.com>
2014-06-18 17:57:40 +0800

    Update LagFixer.php

--- LagFixer/LagFixer.php
+++ LagFixer/LagFixer.php
@@ -14,16 +14,47 @@ class LagFixer implements Plugin{
 		$this->api = $api;
 	}
 	public function init(){
-		$this->api->console->register("show", "<player|all> show an invisible player or attempt to resend all players in your world to you", array($this, "showCmd"));
+		$this->api->console->register("show", "<player|-all> show an invisible player or attempt to resend all players in your world to you", array($this, "showCmd"));
 		$this->api->console->register("realhealth", "Send you real health to you", array($this, "rhCmd"));
 		$this->api->ban->cmdWhitelist("show");
 		$this->api->ban->cmdWhitelist("realhealth");
 	}
-	public function showCmd($c, $a, $p){
+	public function showCmd($c, $a, $issuer){
 		if(!isset($a[0])){
 			return "Usage: /show <player|all> show an invisible player or attempt to resend all players in your world to you";
 		}
-		
+		if($a[0] !== "-all"){
+			$player = $this->api->player->get($a[0]);
+			if(!($player instanceof Player)){
+				return "Player $a[0] not found!";
+			}
+			$pk = new AddPlayerPacket;
+			$pk->clientID = 0;
+			$pk->username = $player->username;
+			$e = $player->entity;
+			$pk->eid = $e->eid
+			$pk->x = $e->x;
+			$pk->y = $e->y;
+			$pk->z = $e->z;
+			$pk->pitch = $e->pitch;
+			$pk->yaw = $e->yaw;
+			$pk->unknown1 = 0;
+			$pk->unknown2 = 0;
+			$pk->metadata = $e->getMetadata();
+			if($player->entity->level->getName() !== $issuer->entity->level->getName()){
+				$pk->x = 512;
+				$pk->y = 512;
+				$pk->z = 512;
+			}
+			$issuer->dataPacket($pk);
+			$pk = new PlayerEquipmentPacket;
+			$pk->eid = $e->eid;
+			$pk->item = $player->getSlot($player->slot)->getID();
+			$pk->meta = $player->getSlot($player->slot)->getMetadata();
+			$pk->slot = 0;
+			$issuer->dataPacket($pk);
+			$player->sendArmor($issuer);
+		}
 	}
 	public function rhCmd($c, $a, $p){
 		
